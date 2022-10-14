23392069f01877aca72d562d16e3a4e7c97c511d
Zikoha Derrick <zikoejeagwu@gmail.com>
2022-10-13 19:20:06 -0700

    puts printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main-prints exactly "with proper grammar, but the outcome is a piece of art,
- *   * Return: should return zero
- *    */
+ * main - entry point
+ * Return: should return zero
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
