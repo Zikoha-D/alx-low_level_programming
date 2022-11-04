f146fb265528fa2458a8b4c9db2e28a4cf831284
Zikoha Derrick <zikoejeagwu@gmail.com>
2022-11-04 14:50:10 -0700

    Advance Tasks

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,35 +1,21 @@
-#include "main.h"
-
-int _atoi(char *s);
-int multiply_two_nums(int num1, int num2);
-void print_number(int n);
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - Entry point
-*
-* @argc: Length of @argv
-*
-* @argv: Array of strings of the arguments of this program
-*
-* Return: 0, Success
-*/
-
-int main(int argc, char *argv[])
+ * main - nultiplies two numbers
+ * @argc: argument count
+ * @argv: array of arguments
+ *
+ * Return: product of number on success, otherwise 1
+ */
+int main(int argc, char  *argv[])
 {
 	if (argc == 3)
 	{
-		print_number(multiply_two_nums(_atoi(argv[1]), _atoi(argv[2])));
-		_putchar('\n');
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
-	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		return (0);
-	}
+		printf("Error\n");
+	return (1);
 }
